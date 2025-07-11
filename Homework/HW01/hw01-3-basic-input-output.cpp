75accf69084015893f268ab911ea99f65785a970
0krk01234 <ruizfeez99@gmail.com>
2025-07-11 12:38:09 +0700

    second commit

---
+++ Homework/HW01/hw01-3-basic-input-output.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main () {
+    char FirstName[50] ;
+    char Lastname[50] ;
+    printf("First name: \n");
+    scanf("%s" , FirstName );
+    printf("Last name: \n");
+    scanf("%s" , Lastname);
+    printf("%s  %s tc, Rmutl, Chiang Mai, Thailand " , FirstName , Lastname  ) ;
+    return 0;
+}
